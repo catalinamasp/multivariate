12e890055f8647c39e8d0cc8ef2738bb1afdbe6c
Catalina Mas Pohmajevic <catalinamasp@Catalinas-MacBook-Pro.local>
2021-07-04 15:02:37 +0200

    Linear regression, homoscedasticity and correlation

--- R-Multivariate.R
+++ R-Multivariate.R
@@ -34,3 +34,58 @@ dataset <- rename(dataset, c(EFP = 'Total Ecological Footprint [gha per capita]'
                              LAND = land,
                              AIR = Pollution,
                              EDU = EDUC))
+
+summary(dataset)
+
+
+lm <- lm(dataset$EFP ~ dataset$HRS)
+summary(lm)
+
+#There's a negative effect, for every increase in point of HRS there's a negative effect in Ecological   footprint.
+
+b <- lm$coef
+#beta
+#capture the coefficients that are estimated
+u <- lm$resid
+#residuals to collect residuals
+
+fit <- lm$fitted.values
+
+plot(fit,
+     u,
+     pch = 19,
+     col = "darkblue",
+     xlab = "Fitted Values",
+     ylab = "Residuals",
+     main = "Plot of Residual Variance"
+)
+
+#HETEROSCEIDASTICITY
+
+library(lmtest)
+bptest(lm)
+
+#0.85 > 0.05 
+#We can reject the null hypothesis saying that heterosceidasticity exists
+
+
+
+
+#GDP CAN'T BE NEGATIVE
+
+
+
+#TRADE
+
+dataset$trade <- log(1 + dataset$IMP + dataset$EXP)
+summary(data.nomissing)
+
+# Task 3: Let's check for autocorrelation #
+
+install.packages("dwtest")
+dwtest(lm)
+
+
+
+
+
