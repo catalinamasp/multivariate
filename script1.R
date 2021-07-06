43c19ed42d147ca4d619093eddab0d525df216f3
simonedeluca <delucasim@hotmail.it>
2021-07-06 18:45:42 +0200

    eco.sector progress

--- script1.R
+++ script1.R
@@ -50,7 +50,7 @@ summary(mod.mult)
 
 #Only GDPcap significant
 
-# 2.Regressing the ecological footprint on human rights score, trade and GDP per capita.
+# 2.Regression ECONOMICS: the ecological footprint on human rights score, trade and GDP per capita.
 mod.mult1 <- lm(EFP ~ HRS + TRADE + GDPcap, data = data.nomiss)
 summary(mod.mult1)
 
@@ -69,6 +69,32 @@ summary(mod.mult_try)
 eco.sector <- readxl::read_xlsx("GDP_by_sector_wb.xlsx")
 eco.sector <- na.omit(eco.sector)
 
+#Cleaning the dataset
+eco.sector$Infrastructure <- (eco.sector$Industry - eco.sector$Manufactoring)
+eco.sector <- eco.sector[,-3]
+eco.sector <- rename(eco.sector, Primary = 'Agriculture, forestry, and fishing')
+
+#Creating categorical variables according strongest sector (% of GDP). 
+
+eco.sector$Main <- ifelse(eco.sector$Primary >= eco.sector$Manufactoring &
+                            eco.sector$Primary >= eco.sector$Infrastructure &
+                            eco.sector$Primary >= eco.sector$Services,
+                          "Primary",
+                          ifelse(eco.sector$Manufactoring >= eco.sector$Primary &
+                            eco.sector$Manufactoring >= eco.sector$Infrastructure &
+                            eco.sector$Manufactoring >= eco.sector$Services,
+                          "Manufactoring",
+                          ifelse(eco.sector$Infrastructure >= eco.sector$Primary &
+                            eco.sector$Infrastructure >= eco.sector$Manufactoring &
+                            eco.sector$Infrastructure >= eco.sector$Services,
+                          "Infrastructure",
+                          "Services")
+                          ))
+                          
+# As expected, the sector of Services accounts for the largest part of GDP in nearly all the countries in the dataset.
+# Does an economy mainly service-oriented have an impact on the environment health of a country?
+
+mod.mult2 <- lm(data.nomiss$EFP ~  )
 
 #CO2 PER CAPITA
 
